37ce793c7d9a50f78592fe8c62415a6009e01e0f
Mariana Torres Torres <mari.torret@gmail.com>
2021-03-23 20:18:26 +0000

    add some get/set functions to tweet class

--- sentimental/src/main/java/com/google/sps/data/Tweet.java
+++ sentimental/src/main/java/com/google/sps/data/Tweet.java
@@ -5,12 +5,32 @@ public final class Tweet {
   private final long id;
   private final String text;
   private final long timestamp;
-  private final float index;
+  private float index;
 
-  public Tweet(long _id, String _text, long _timestamp, float _index) {
+  public Tweet(long _id, String _text, long _timestamp) {
     this.id = _id;
     this.text = _text;
     this.timestamp = _timestamp;
-    this.index = _index;
+    this.index = 0.0;
+  }
+
+  public String getID() {
+      return this.id;
+  }
+
+  public String getText() {
+      return this.text;
+  }
+
+  public String getTimestamp() {
+      return this.timestamp;
+  }
+
+  public String getIndex() {
+      return this.index;
+  }
+
+  public void setIndex(float _index) {
+      this.index = _index;
   }
 }
