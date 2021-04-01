e072cea897207a9bfaee4922b09c28b50c4fa256
Carolina <caro99gm@gmail.com>
2021-04-01 20:50:04 +0000

    Use sentiment analysis with the collected tweets

--- sentimental/src/main/webapp/sentiment.js
+++ sentimental/src/main/webapp/sentiment.js
@@ -1,13 +1,15 @@
 async function sentimentText() {
-  const text = document.getElementById('intro').innerHTML;
+  console.log("Entre");
+  //const text = document.getElementById('intro').innerHTML;
   
-  const params = new URLSearchParams();
+  //const params = new URLSearchParams();
 
-  params.append('message', text);
-  const responseFromServer = await fetch('/sentiment', {method: "POST", body: params});
+  //params.append('message', text);
+  const responseFromServer = await fetch('/sentiment', {method: "POST"});//, body: params});
     
   const result = document.getElementById('sentimentResult');
 
   result.innerHTML = await responseFromServer.text();
+  console.log(result);
 }
  
