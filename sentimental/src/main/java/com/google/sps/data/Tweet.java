4465d114a2c28661c8295a09c63e92023e1599e5
Jose Carlos <jacosta291@gmail.com>
2021-04-08 19:08:56 +0000

    Add search parameters relating to feminism, add search for the 3 biggest cities in Mexico, modify datastore service to handle city (pending fix), modify tweet class to handle city

--- sentimental/src/main/java/com/google/sps/data/Tweet.java
+++ sentimental/src/main/java/com/google/sps/data/Tweet.java
@@ -6,16 +6,18 @@ public final class Tweet {
   private final String text;
   private final long timestamp;
   private double sentimentScore;
+  private String city;
 
-  public Tweet(long _id, String _text, long _timestamp, double _sentimentScore) {
+  public Tweet(long _id, String _text, long _timestamp, double _sentimentScore, String _city) {
     this.id = _id;
     this.text = _text;
     this.timestamp = _timestamp;
     this.sentimentScore = _sentimentScore;
+    this.city = _city;
   }
 
   public Tweet(long _id, String _text, long _timestamp) {
-    this(_id, _text, _timestamp, 0);
+    this(_id, _text, _timestamp, 0, "none");
   }
 
   public long getID() {
@@ -36,5 +38,13 @@ public final class Tweet {
 
   public void setSentimentScore(float sentimentScore) {
       this.sentimentScore = sentimentScore;
+  }
+
+  public String getCity(){
+      return this.city;
+  }
+
+  public void setCity(String city){
+      this.city = city;
   }
 }
--- sentimental/src/main/java/com/google/sps/twitterapi/API.java
+++ sentimental/src/main/java/com/google/sps/twitterapi/API.java
@@ -5,22 +5,28 @@ import twitter4j.*;
 import twitter4j.auth.*;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class API{
     Twitter twitterAPI = TwitterFactory.getSingleton();
-
+    HashMap<String, GeoLocation> mexicanCities = populateCities();
     public ArrayList<Tweet> searchTweets(String keyword){
         ArrayList<Tweet> tweets = new ArrayList<>();
+        
         try {
-
-            QueryResult result = search(keyword);
-            List<Status> rawTweets = extractTweets(result);
+            for (String city : mexicanCities.keySet()) {
+                QueryResult result = search(keyword, city);
+                List<Status> rawTweets = extractTweets(result);
 
             for (Status rawTweet: rawTweets
                  ) {
                 Tweet newTweet = new Tweet(rawTweet.getId(), rawTweet.getText(), rawTweet.getCreatedAt().getTime());
+                newTweet.setCity(city);
                 tweets.add(newTweet);
+                System.out.println(newTweet.getCity());
             }
+            }
+           
 
         } catch (TwitterException e) {
             System.out.println("Error while performing search: "+e.getErrorMessage());
@@ -29,12 +35,27 @@ public class API{
 
     }
 
-    private QueryResult search(String keyword) throws TwitterException {
+    private QueryResult search(String keyword, String city) throws TwitterException {
         Query query = new Query(keyword);
+        GeoLocation geo = mexicanCities.get(city);
+        query.setGeoCode(geo, 50, Query.Unit.km);
         return twitterAPI.search(query);
     }
 
     private List<Status> extractTweets(QueryResult result){
         return result.getTweets();
+    }
+
+    private HashMap<String, GeoLocation> populateCities(){
+        HashMap<String, GeoLocation> cities = new HashMap<>();
+        GeoLocation geo = new GeoLocation(19.42847, -99.12766);
+        cities.put("Ciudad de Mexico", geo);
+        geo = new GeoLocation(25.67507, -100.31847);
+        cities.put("Monterrey", geo);
+        geo = new GeoLocation(20.66682, -103.39182);
+        cities.put("Guadalajara", geo);
+
+
+        return cities;
     }
 }
