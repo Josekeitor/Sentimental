5c16c03fad62e2c6004b487653efe87406238bb5
Mariana Torres Torres <mari.torret@gmail.com>
2021-04-08 18:02:20 +0000

    hard-coded heatmap works

--- sentimental/src/main/webapp/map.js
+++ sentimental/src/main/webapp/map.js
@@ -1,15 +1,35 @@
 // Initialize and add the map
 function initMap() {
-  // The location of Uluru
-  const mexicoCity = { lat: 19.432608, lng: -99.133209 };
-  // The map, centered at Uluru
+  /* Data points defined as a mixture of WeightedLocation and LatLng objects */
+  let heatMapData = [
+    {location: new google.maps.LatLng(37.782, -122.447), weight: 0.5},
+    new google.maps.LatLng(37.782, -122.445),
+    {location: new google.maps.LatLng(37.782, -122.443), weight: 2},
+    {location: new google.maps.LatLng(37.782, -122.441), weight: 3},
+    {location: new google.maps.LatLng(37.782, -122.439), weight: 2},
+    new google.maps.LatLng(37.782, -122.437),
+    {location: new google.maps.LatLng(37.782, -122.435), weight: 0.5},
+    {location: new google.maps.LatLng(37.785, -122.447), weight: 3},
+    {location: new google.maps.LatLng(37.785, -122.445), weight: 2},
+    new google.maps.LatLng(37.785, -122.443),
+    {location: new google.maps.LatLng(37.785, -122.441), weight: 0.5},
+    new google.maps.LatLng(37.785, -122.439),
+    {location: new google.maps.LatLng(37.785, -122.437), weight: 2},
+    {location: new google.maps.LatLng(37.785, -122.435), weight: 3}
+  ];
+  let mexicoCity = { lat: 19.432608, lng: -99.133209 };
+  // The map, centered at Mexico City
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 5,
     center: mexicoCity,
   });
-  // The marker, positioned at Uluru
+  // The marker, positioned at Mexico City
   const marker = new google.maps.Marker({
     position: mexicoCity,
     map: map,
   });
+  let heatmap = new google.maps.visualization.HeatmapLayer({
+    data: heatMapData
+  });
+  heatmap.setMap(map);
 }
